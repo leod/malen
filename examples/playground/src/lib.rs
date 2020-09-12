acd089784842725e59a2d32d96e1e844a20cd08e
leod <subtle.frustration@proton.me>
2020-09-12 21:40:32 +0200

    Log some first inputs

--- examples/playground/src/lib.rs
+++ examples/playground/src/lib.rs
@@ -1,10 +1,29 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
+use webglee::Event::*;
+
 #[wasm_bindgen(start)]
 pub fn main() {
     web_logger::init();
     log::info!("Hi, starting the example");
 
-    let context = webglee::Context::from_canvas_id("canvas").unwrap();
+    let mut context = webglee::Context::from_canvas_id("canvas").unwrap();
     log::info!("Initialized webglee context");
+
+    webglee::main_loop(move |dt, _running| {
+        for event in context.input_mut().pop_event() {
+            match event {
+                Focused => {
+                    log::info!("got focus");
+                }
+                Unfocused => {
+                    log::info!("lost focus");
+                }
+                KeyPressed(key) => {
+                    log::info!("key pressed: {:?}", key);
+                }
+                _ => (),
+            }
+        }
+    })
 }
