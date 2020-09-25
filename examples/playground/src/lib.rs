b3f4a2a90b03bb4cdcd15a4b47e8b44c57657e14
leod <subtle.frustration@proton.me>
2020-09-26 00:14:48 +0200

    Render some walls in the example and fix bugs in batching

--- examples/playground/src/lib.rs
+++ examples/playground/src/lib.rs
@@ -1,30 +1,111 @@
+use rand::Rng;
+use rand_distr::{Distribution, Normal};
 use wasm_bindgen::prelude::wasm_bindgen;
+
 use webglee::Event::*;
 use webglee::{
     draw::{Batch, ColorPass, ColorVertex, Quad},
-    Color, Matrix3, Point3, Vector2,
+    Color, Context, Error, Matrix3, Point2, Point3, Vector2,
 };
+
+struct Wall {
+    center: Point2,
+    size: Vector2,
+}
+
+struct Game {
+    color_pass: ColorPass,
+    tri_batch: Batch<ColorVertex>,
+    line_batch: Batch<ColorVertex>,
+
+    walls: Vec<Wall>,
+}
+
+impl Game {
+    pub fn new(ctx: &Context) -> Result<Game, Error> {
+        let color_pass = ColorPass::new(ctx)?;
+        let tri_batch = Batch::<ColorVertex>::new_triangles(ctx)?;
+        let line_batch = Batch::<ColorVertex>::new_lines(ctx)?;
+
+        let mut rng = rand::thread_rng();
+        let normal = Normal::new(150.0, 50.0).unwrap();
+        let walls = (0..2)
+            .map(|_| {
+                let center = Point2::new(rng.gen(), rng.gen()) * 320.0;
+                let size = Vector2::new(normal.sample(&mut rng), normal.sample(&mut rng));
+
+                Wall { center, size }
+            })
+            .collect();
+
+        Ok(Game {
+            color_pass,
+            tri_batch,
+            line_batch,
+            walls,
+        })
+    }
+
+    pub fn render_quad_with_outline(&mut self, center: Point2, size: Vector2, color: Color) {
+        let quad = Quad::axis_aligned(Point3::new(center.x, center.y, 0.5), size);
+
+        self.tri_batch.push_quad(&quad, color);
+        self.line_batch
+            .push_quad_outline(&quad, Color::new(0.0, 0.0, 0.0, 1.0));
+    }
+
+    pub fn draw(&mut self, ctx: &Context) {
+        let screen = ctx.screen();
+        let golem_ctx = ctx.golem_context();
+
+        golem_ctx.set_viewport(0, 0, screen.size.x as u32, screen.size.y as u32);
+        golem_ctx.set_clear_color(1.0, 1.0, 0.0, 1.0);
+        golem_ctx.clear();
+
+        self.tri_batch.clear();
+        self.line_batch.clear();
+
+        self.render_quad_with_outline(
+            Point2::new(320.0, 240.0),
+            Vector2::new(100.0, 100.0),
+            Color::new(1.0, 0.0, 0.0, 1.0),
+        );
+
+        for i in 0..self.walls.len() {
+            self.render_quad_with_outline(
+                self.walls[i].center,
+                self.walls[i].size,
+                Color::new(0.8, 0.8, 0.8, 1.0),
+            )
+        }
+
+        self.color_pass
+            .draw_batch(
+                &screen.orthographic_projection(),
+                &Matrix3::identity(),
+                &mut self.tri_batch,
+            )
+            .unwrap();
+        self.color_pass
+            .draw_batch(
+                &screen.orthographic_projection(),
+                &Matrix3::identity(),
+                &mut self.line_batch,
+            )
+            .unwrap();
+    }
+}
 
 #[wasm_bindgen(start)]
 pub fn main() {
+    console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).unwrap();
     log::info!("Hi, starting the example");
 
-    let ctx = webglee::Context::from_canvas_id("canvas").unwrap();
+    let ctx = Context::from_canvas_id("canvas").unwrap();
     log::info!("Initialized webglee context");
 
-    let mut color_pass = ColorPass::new(&ctx).unwrap();
-    let mut tri_batch = Batch::<ColorVertex>::new_triangles(&ctx).unwrap();
-    let mut line_batch = Batch::<ColorVertex>::new_lines(&ctx).unwrap();
-
-    tri_batch.push_quad(
-        &Quad::axis_aligned(Point3::new(320.0, 240.0, 0.5), Vector2::new(100.0, 100.0)),
-        Color::new(1.0, 0.0, 0.0, 1.0),
-    );
-    line_batch.push_quad_outline(
-        &Quad::axis_aligned(Point3::new(320.0, 240.0, 0.5), Vector2::new(100.0, 100.0)),
-        Color::new(0.0, 0.0, 0.0, 1.0),
-    );
+    let mut game = Game::new(&ctx).unwrap();
 
     ctx.main_loop(move |ctx, _dt, events, _running| {
         for event in events {
@@ -45,27 +126,7 @@ pub fn main() {
             }
         }
 
-        let screen = ctx.screen();
-        let golem_ctx = ctx.golem_context();
-
-        golem_ctx.set_viewport(0, 0, screen.size.x as u32, screen.size.y as u32);
-        golem_ctx.set_clear_color(1.0, 1.0, 0.0, 1.0);
-        golem_ctx.clear();
-
-        color_pass
-            .draw_batch(
-                &screen.orthographic_projection(),
-                &Matrix3::identity(),
-                &mut tri_batch,
-            )
-            .unwrap();
-        color_pass
-            .draw_batch(
-                &screen.orthographic_projection(),
-                &Matrix3::identity(),
-                &mut line_batch,
-            )
-            .unwrap();
+        game.draw(&ctx);
     })
     .unwrap();
 }
