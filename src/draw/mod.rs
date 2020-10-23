50a7f768e5748b5b56f8b636704d13a125ec4506
leod <subtle.frustration@proton.me>
2020-10-23 23:38:58 +0200

    Start working on DPI things, and use f32 textures

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -24,16 +24,44 @@ pub struct Draw {
 
 impl Draw {
     pub fn new(canvas: HtmlCanvasElement, golem_ctx: golem::Context) -> Result<Self, Error> {
+        match_hidpi_canvas_size(&canvas);
+
         Ok(Draw { canvas, golem_ctx })
     }
 
     pub fn screen(&self) -> Screen {
         Screen {
             size: na::Vector2::new(self.canvas.width(), self.canvas.height()),
+            device_pixel_ratio: device_pixel_ratio(),
         }
     }
 
     pub fn golem_ctx(&self) -> &golem::Context {
         &self.golem_ctx
     }
+}
+
+fn device_pixel_ratio() -> f64 {
+    let window = web_sys::window().expect("Failed to obtain window");
+    window.device_pixel_ratio()
+}
+
+fn match_hidpi_canvas_size(canvas: &HtmlCanvasElement) {
+    let scale_factor = device_pixel_ratio();
+
+    let start_width = canvas.width();
+    let start_height = canvas.height();
+
+    canvas.set_width((start_width as f64 * scale_factor).round() as u32);
+    canvas.set_height((start_height as f64 * scale_factor).round() as u32);
+
+    set_canvas_style_property(canvas, "width", &format!("{}px", start_width));
+    set_canvas_style_property(canvas, "height", &format!("{}px", start_height));
+}
+
+fn set_canvas_style_property(canvas: &HtmlCanvasElement, property: &str, value: &str) {
+    let style = canvas.style();
+    style
+        .set_property(property, value)
+        .expect(&format!("Failed to set {}", property));
 }
