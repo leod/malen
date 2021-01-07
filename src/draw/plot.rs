a73ff90caf06e60df39e9c7e245a88032e04df6d
Leonard Dahlmann <leo.dahlmann@gmail.com>
2021-01-08 00:07:35 +0100

    Implement tics rendering

--- src/draw/plot.rs
+++ src/draw/plot.rs
@@ -1,31 +1,33 @@
-use nalgebra::{Matrix2, Matrix3, Point2, Vector2};
+use nalgebra::{convert, Matrix2, Matrix3, Point2, Point3, Vector2};
 
 use crate::{
     draw::{ColPass, ColVertex, Font, LineBatch, TextBatch, TriBatch},
     AaRect, Canvas, Color4, Error,
 };
 
-const AXIS_MARGIN: f32 = 30.0;
+const AXIS_MARGIN: f64 = 70.0;
+const TICK_SIZE: f64 = 7.5;
 
 #[derive(Debug, Clone)]
 pub struct LinePlotData {
     pub caption: String,
     pub color: Color4,
-    pub points: Vec<(f32, f32)>,
+    pub points: Vec<(f64, f64)>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Axis {
     pub label: String,
-    pub range: Option<(f32, f32)>,
-    pub tics: f32,
+    pub range: Option<(f64, f64)>,
+    pub tics: f64,
+    pub tic_precision: usize,
 }
 
 impl Axis {
-    pub fn get_range(&self, data: impl Iterator<Item = f32>) -> (f32, f32) {
+    pub fn get_range(&self, data: impl Iterator<Item = f64>) -> (f64, f64) {
         self.range.unwrap_or_else(|| {
-            let mut min = std::f32::MAX;
-            let mut max = std::f32::MIN;
+            let mut min = std::f64::MAX;
+            let mut max = std::f64::MIN;
 
             for item in data {
                 min = min.min(item);
@@ -37,7 +39,7 @@ impl Axis {
     }
 }
 
-fn clip_to_ranges(p: Point2<f32>, x_range: (f32, f32), y_range: (f32, f32)) -> Point2<f32> {
+fn clip_to_ranges(p: Point2<f64>, x_range: (f64, f64), y_range: (f64, f64)) -> Point2<f64> {
     Point2::new(
         p.x.max(x_range.0).min(x_range.1),
         p.y.max(y_range.0).min(y_range.1),
@@ -46,11 +48,12 @@ fn clip_to_ranges(p: Point2<f32>, x_range: (f32, f32), y_range: (f32, f32)) -> P
 
 #[derive(Debug, Clone)]
 pub struct Plot {
-    pub size: Vector2<f32>,
+    pub size: Vector2<f64>,
     pub x_axis: Axis,
     pub y_axis: Axis,
     pub axis_color: Color4,
     pub background_color: Option<Color4>,
+    pub text_color: Color4,
     pub lines: Vec<LinePlotData>,
 }
 
@@ -78,11 +81,6 @@ impl Plotting {
         transform: &Matrix3<f32>,
         plot: &Plot,
     ) -> Result<(), Error> {
-        // Flip Y axis so that (0,0) is at the bottom-right of the screen again.
-        // (This is assuming that the caller uses
-        // ScreenGeom::orthographic_projection.)
-        let transform = Matrix3::new_nonuniform_scaling(&Vector2::new(1.0, -1.0)) * transform;
-
         // Reset batch contents from previous draw calls.
         self.tri_batch.clear();
         self.line_batch.clear();
@@ -91,7 +89,7 @@ impl Plotting {
         // Render plot background.
         if let Some(background_color) = plot.background_color {
             self.tri_batch.push_quad(
-                &AaRect::from_top_left(Point2::origin(), plot.size).into(),
+                &AaRect::from_top_left(Point2::origin(), convert(plot.size)).into(),
                 0.0,
                 background_color,
             );
@@ -102,7 +100,8 @@ impl Plotting {
         let plot_size = plot.size - 2.0 * plot_offset;
 
         self.line_batch.push_quad_outline(
-            &AaRect::from_top_left(Point2::origin() + plot_offset, plot_size).into(),
+            &AaRect::from_top_left(convert(Point2::origin() + plot_offset), convert(plot_size))
+                .into(),
             0.1,
             plot.axis_color,
         );
@@ -124,20 +123,99 @@ impl Plotting {
             plot_size.y / (y_range.1 - y_range.0),
         ));
 
-        let map_point = |(x, y): (f32, f32)| {
-            let pos = Point2::new(x, y);
+        let map_point = |pos: Point2<f64>| -> Point2<f32> {
             let clipped = clip_to_ranges(pos, x_range, y_range);
             let shifted = clipped - Vector2::new(x_range.0, y_range.0);
             let scaled = axis_scale * shifted;
             let margined = scaled + plot_offset;
-            margined
+            let flipped = Point2::new(margined.x, plot.size.y - margined.y);
+            convert(flipped)
         };
+
+        // Draw tics at the axes.
+        let mut render_tics = |range: (f64, f64), tics: f64, tic_precision: usize, is_x: bool| {
+            if range.0 >= range.1 || tics == 0.0 {
+                return;
+            }
+
+            let round_to_multiple = |x: f64| (x / tics).round() * tics;
+
+            let axis = if is_x {
+                Vector2::new(1.0, 0.0)
+            } else {
+                Vector2::new(0.0, 1.0)
+            };
+            let normal = if is_x {
+                Vector2::new(0.0f32, -1.0)
+            } else {
+                Vector2::new(1.0, 0.0f32)
+            };
+
+            let mut current_offset = round_to_multiple(range.0);
+            while current_offset <= round_to_multiple(range.1) {
+                let text = &format!("{:.*}", tic_precision, current_offset);
+                let text_size = font.text_size(11.0, &text);
+
+                let pos = map_point(Point2::origin() + current_offset * axis);
+                let shifted_pos = if is_x {
+                    Point2::new(pos.x - text_size.x / 2.0, pos.y + 6.0)
+                } else {
+                    Point2::new(pos.x - 6.0 - text_size.x, pos.y - text_size.y / 2.0 - 3.0)
+                };
+                let shifted_pos = Point3::new(shifted_pos.x, shifted_pos.y, 0.2);
+
+                font.write(
+                    11.0,
+                    convert(shifted_pos),
+                    plot.text_color,
+                    &text,
+                    &mut self.text_batch,
+                );
+
+                if is_x {
+                    self.line_batch.push_line(
+                        pos,
+                        pos + normal * TICK_SIZE as f32,
+                        0.4,
+                        plot.axis_color,
+                    );
+                    self.line_batch.push_line(
+                        pos - Vector2::new(0.0, plot_size.y as f32),
+                        pos - Vector2::new(0.0, plot_size.y as f32) - normal * TICK_SIZE as f32,
+                        0.4,
+                        plot.axis_color,
+                    );
+                } else {
+                    self.line_batch.push_line(
+                        pos,
+                        pos + normal * TICK_SIZE as f32,
+                        0.4,
+                        plot.axis_color,
+                    );
+                    self.line_batch.push_line(
+                        pos + Vector2::new(plot_size.x as f32, 0.0),
+                        pos + Vector2::new(plot_size.x as f32, 0.0) - normal * TICK_SIZE as f32,
+                        0.4,
+                        plot.axis_color,
+                    );
+                }
+
+                current_offset = round_to_multiple(current_offset + tics);
+            }
+        };
+
+        render_tics(x_range, plot.x_axis.tics, plot.x_axis.tic_precision, true);
+        render_tics(y_range, plot.y_axis.tics, plot.y_axis.tic_precision, false);
 
         // Render each of the lines.
         for line in plot.lines.iter() {
             for (p, q) in line.points.iter().zip(line.points.iter().skip(1)) {
-                self.line_batch
-                    .push_line(map_point(*p), map_point(*q), 0.2, line.color);
+                self.line_batch.push_line(
+                    map_point(Point2::new(p.0, p.1)),
+                    map_point(Point2::new(q.0, q.1)),
+                    0.3,
+                    line.color,
+                );
             }
         }
 
@@ -149,16 +227,5 @@ impl Plotting {
         font.draw(canvas, &transform, &self.text_batch.draw_unit())?;
 
         Ok(())
-    }
-
-    fn render_tics(
-        &mut self,
-        axis: &Axis,
-        start_pos: Point2<f32>,
-        delta: Vector2<f32>,
-        offset: Vector2<f32>,
-        n: usize,
-        font: &Font,
-    ) {
     }
 }
