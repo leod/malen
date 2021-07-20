57592c3bb5b9fddb5acfb173dd95abc9ba3a4937
leod <subtle.frustration@proton.me>
2021-07-20 23:59:14 +0200

    Pass absolute timestamp rather than dt
    This is to make sure we do not get clock drift from accumulating errors

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc, time::Duration};
+use std::{cell::RefCell, rc::Rc};
 
 use wasm_bindgen::{closure::Closure, JsCast};
 
@@ -24,7 +24,7 @@ use crate::Error;
 /// - Render the game.
 pub fn main_loop<F>(mut callback: F) -> Result<(), Error>
 where
-    F: FnMut(Duration, &mut bool) + 'static,
+    F: FnMut(f64, &mut bool) + 'static,
 {
     // Source:
     // https://github.com/grovesNL/glow/blob/2d42c5b105d979efe764191b5b1ce78fab99ffcf/src/web_sys.rs#L3258
@@ -38,22 +38,13 @@ where
 
     let f = Rc::new(RefCell::new(None));
 
-    let mut last_timestamp = None;
     let mut running = true;
 
     *f.borrow_mut() = Some(Closure::wrap(Box::new({
         let f = f.clone();
 
-        move |timestamp: f64| {
-            let dt = last_timestamp.map_or(Duration::from_secs(0), |last_timestamp: f64| {
-                let dt_ms = (timestamp - last_timestamp).max(0.0);
-                let dt_secs = dt_ms / 1000.0;
-
-                Duration::from_secs_f64(dt_secs)
-            });
-            last_timestamp = Some(timestamp);
-
-            callback(dt, &mut running);
+        move |timestamp_millis: f64| {
+            callback(timestamp_millis / 1000.0f64, &mut running);
 
             if !running {
                 let _ = f.borrow_mut().take();
