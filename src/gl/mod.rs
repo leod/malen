f9ab5d9758bdc12b719c6909b409cb018e11d9a1
leod <subtle.frustration@proton.me>
2021-12-28 23:19:52 +0100

    Add blending paramters

--- src/gl/mod.rs
+++ src/gl/mod.rs
@@ -1,11 +1,17 @@
 #[macro_use]
 mod vertex;
 
+mod blend;
 mod context;
+mod draw_params;
+mod element_buffer;
 mod program;
 mod vertex_buffer;
 
+pub use blend::{Blend, BlendEquation, BlendFactor, BlendFunc, BlendOp};
 pub use context::Context;
+pub use draw_params::DrawParams;
+pub use element_buffer::ElementBuffer;
 pub use program::Program;
-pub use vertex::{attribute, Attribute, DataType, ElementType, Vertex};
+pub use vertex::{attribute, Attribute, DataType, ValueType, Vertex};
 pub use vertex_buffer::VertexBuffer;
