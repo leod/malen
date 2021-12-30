0642b17158f47fc05193937472dd788435982f4f
Leonard Dahlmann <leo.dahlmann@gmail.com>
2021-12-30 19:47:58 +0100

    Add vertices for geometry

--- src/geometry/geometry.rs
+++ src/geometry/geometry.rs
@@ -2,6 +2,8 @@ use nalgebra::Point2;
 
 use crate::{Rect, RotatedRect, Color4, gl::{Vertex, PrimitiveMode}};
 
+use super::SpriteVertex;
+
 pub trait PrimitiveTag {
     fn primitive_mode() -> PrimitiveMode;
 }
@@ -9,19 +11,7 @@ pub trait PrimitiveTag {
 pub struct TriangleTag {
 }
 
-impl TriangleTag {
-    fn primitive_mode() -> PrimitiveMode {
-        PrimitiveMode::Triangle
-    }
-}
-
 pub struct LineTag {
-}
-
-impl PrimitiveTag for LineTag {
-    fn primitive_mode() -> PrimitiveMode {
-        PrimitiveMode::Line
-    }
 }
 
 pub trait Geometry<Tag: PrimitiveTag> {
@@ -30,28 +20,53 @@ pub trait Geometry<Tag: PrimitiveTag> {
     fn write(&self, vertices: &mut Vec<Self::Vertex>, elements: &mut Vec<u32>);
 }
 
+#[derive(Debug, Copy, Clone)]
 pub struct Sprite {
     pub rect: RotatedRect,
     pub z: f32,
     pub uv: Rect,
 }
 
-pub struct ColSprite {
+#[derive(Debug, Copy, Clone)]
+pub struct ColoredSprite {
     pub rect: RotatedRect,
     pub z: f32,
     pub uv: Rect,
     pub color: Color4,
 }
 
-pub struct ColRect {
+#[derive(Debug, Copy, Clone)]
+pub struct ColoredRotatedRect {
    pub rect: RotatedRect,
    pub z: f32,
    pub color: Color4,
 }
 
-pub struct ColLine {
+#[derive(Debug, Copy, Clone)]
+pub struct ColoredLine {
     pub a: Point2<f32>,
     pub b: Point2<f32>,
     pub z: f32,
     pub color: Color4,
+}
+
+impl PrimitiveTag for TriangleTag {
+    fn primitive_mode() -> PrimitiveMode {
+        PrimitiveMode::Triangle
+    }
+}
+
+impl PrimitiveTag for LineTag {
+    fn primitive_mode() -> PrimitiveMode {
+        PrimitiveMode::Line
+    }
+}
+
+impl Geometry<TriangleTag> for Sprite {
+    type Vertex = SpriteVertex;
+
+    fn write(&self, vertices: &mut Vec<Self::Vertex>, elements: &mut Vec<u32>) {
+        let corners = self.rect.corners();
+        let uv_corners = self.uv.corners();
+    }
 }
---
+++ src/geometry/vertex.rs
@@ -0,0 +1,56 @@
+use nalgebra::{Point2, Point3};
+
+use bytemuck_derive::{Pod, Zeroable};
+use bytemuck::{offset_of, Zeroable};
+
+use crate::{Color4, gl::{attribute, Vertex, Attribute}};
+
+#[derive(Debug, Clone, Copy, Zeroable, Pod)]
+#[repr(C)]
+pub struct SpriteVertex {
+    pub position: Point3<f32>,
+    pub uv: Point2<f32>,
+}
+
+#[derive(Debug, Clone, Copy, Zeroable, Pod)]
+#[repr(C)]
+pub struct ColoredSpriteVertex {
+    pub position: Point3<f32>,
+    pub uv: Point2<f32>,
+    pub color: Color4,
+}
+
+#[derive(Debug, Clone, Copy, Zeroable, Pod)]
+#[repr(C)]
+pub struct ColoredVertex {
+    pub position: Point3<f32>,
+    pub color: Color4,
+}
+
+impl Vertex for SpriteVertex {
+    fn attributes() -> Vec<Attribute> {
+        vec![
+            attribute::<Point3<f32>>("v_position", offset_of!(Self::zeroed(), Self, position)),
+            attribute::<Point2<f32>>("v_uv", offset_of!(Self::zeroed(), Self, uv))
+        ]
+    }
+}
+
+impl Vertex for ColoredSpriteVertex {
+    fn attributes() -> Vec<Attribute> {
+        vec![
+            attribute::<Point3<f32>>("v_position", offset_of!(Self::zeroed(), Self, position)),
+            attribute::<Point2<f32>>("v_uv", offset_of!(Self::zeroed(), Self, uv)),
+            attribute::<Color4>("v_color", offset_of!(Self::zeroed(), Self, color))
+        ]
+    }
+}
+
+impl Vertex for ColoredVertex {
+    fn attributes() -> Vec<Attribute> {
+        vec![
+            attribute::<Point3<f32>>("v_position", offset_of!(Self::zeroed(), Self, position)),
+            attribute::<Color4>("v_color", offset_of!(Self::zeroed(), Self, color))
+        ]
+    }
+}
