d38fbdf6f69b0586e5dbbd965abd501298d32676
leod <subtle.frustration@proton.me>
2021-12-30 19:47:58 +0100

    Add vertices for geometry

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,6 +1,8 @@
 mod batch;
 mod buffer;
 mod geometry;
+mod vertex;
 
 pub use buffer::GeometryBuffer;
 pub use geometry::{Geometry, LineTag, PrimitiveTag, TriangleTag};
+pub use vertex::{ColoredSpriteVertex, ColoredVertex, SpriteVertex};
