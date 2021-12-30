966f29dae4e0b7459aef35fde9b0505bec691b15
Leonard Dahlmann <leo.dahlmann@gmail.com>
2021-12-30 18:39:36 +0100

    Add RotatedRect

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -4,7 +4,7 @@ mod screen;
 mod transforms;
 
 pub use camera::Camera;
-pub use rect::Rect;
+pub use rect::{Rect, RotatedRect};
 pub use screen::Screen;
 pub use transforms::{
     matrix3_to_array, scale_rotate_translate, scale_translate, scale_translate3,
--- src/math/rect.rs
+++ src/math/rect.rs
@@ -1,6 +1,6 @@
 use nalgebra::{Point2, Vector2};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Rect {
     pub center: Point2<f32>,
     pub size: Vector2<f32>,
@@ -30,5 +30,72 @@ impl Rect {
             center: self.center + delta,
             size: self.size,
         }
+    }
+
+    pub fn rotate(self, angle: f32) -> RotatedRect {
+        RotatedRect {
+            center: self.center,
+            size: self.size,
+            angle,
+        }
+    }
+
+    pub fn to_rotated_rect(self) -> RotatedRect {
+        self.rotate(0.0)
+    }
+
+    pub fn corners(self) -> [Point2<f32>; 4] {
+        let dx = Vector2::new(self.size.x / 2.0, 0.0);
+        let dy = Vector2::new(0.0, self.size.y / 2.0);
+
+        [
+            self.center - dx - dy,
+            self.center + dx - dy,
+            self.center + dx + dy,
+            self.center - dx + dy,
+        ]
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct RotatedRect {
+    pub center: Point2<f32>,
+    pub size: Vector2<f32>,
+    pub angle: f32,
+}
+
+impl RotatedRect {
+    pub fn translate(self, delta: Vector2<f32>) -> Self {
+        Self {
+            center: self.center + delta,
+            size: self.size,
+            angle: self.angle,
+        }
+    }
+
+    pub fn rotate(self, angle: f32) -> RotatedRect {
+        RotatedRect {
+            center: self.center,
+            size: self.size,
+            angle,
+        }
+    }
+
+    pub fn corners(self) -> [Point2<f32>; 4] {
+        let off_x = self.size.x / 2.0;
+        let off_y = self.size.y / 2.0;
+
+        let cos = self.angle.cos();
+        let sin = self.angle.sin();
+
+        let dx = Vector2::new(cos * off_x, sin * off_x);
+        let dy = Vector2::new(-sin * off_y, cos * off_y);
+
+        [
+            self.center - dx - dy,
+            self.center + dx - dy,
+            self.center + dx + dy,
+            self.center - dx + dy,
+        ]
     }
 }
