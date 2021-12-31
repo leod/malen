2f3b88100add30e7f2426cb3dfa8cd2f6292d200
leod <subtle.frustration@proton.me>
2021-12-31 13:47:10 +0100

    Split up input mod

---
+++ src/input/input_state.rs
@@ -0,0 +1,33 @@
+use std::collections::BTreeSet;
+
+use super::{Event, Key};
+
+#[derive(Debug, Clone, Default)]
+pub struct InputState {
+    pressed_keys: BTreeSet<Key>,
+}
+
+impl InputState {
+    pub(crate) fn on_event(&mut self, event: &Event) {
+        match event {
+            Event::Unfocused => {
+                self.pressed_keys.clear();
+            }
+            Event::KeyPressed(key) => {
+                self.pressed_keys.insert(*key);
+            }
+            Event::KeyReleased(key) => {
+                self.pressed_keys.remove(key);
+            }
+            _ => (),
+        }
+    }
+
+    pub fn key(&self, key: Key) -> bool {
+        self.pressed_keys.contains(&key)
+    }
+
+    pub fn pressed_keys(&self) -> &BTreeSet<Key> {
+        &self.pressed_keys
+    }
+}
