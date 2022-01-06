91a346ed14dfa3bf7856baa42fde918346681569
leod <subtle.frustration@proton.me>
2022-01-06 20:25:09 +0100

    Make Canvas Rc and add Line

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -3,11 +3,13 @@ mod circle;
 mod rect;
 mod screen;
 mod transforms;
+mod line;
 
 pub use camera::Camera;
 pub use circle::Circle;
 pub use rect::{Rect, RotatedRect};
 pub use screen::Screen;
+pub use line::Line;
 pub use transforms::{
     matrix3_to_array, scale_rotate_translate, scale_translate, scale_translate3,
     translate_rotate_scale,
