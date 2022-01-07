b5154ebd93a90882beeb65d1df3d3c171fcd5cd8
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-01-07 04:15:16 +0100

    Circle and rotated rect occluders

--- src/light/mod.rs
+++ src/light/mod.rs
@@ -1,14 +1,15 @@
 mod color_pass;
 mod data;
+mod occluder;
 mod occluder_batch;
 mod pipeline;
 mod screen_light_pass;
 mod shadow_map_pass;
 
 pub use color_pass::ColorPass;
-pub use data::{
-    GlobalLightParams, GlobalLightParamsBlock, Light, OccluderLine, OccluderLineVertex,
-    OccluderRect,
+pub use data::{GlobalLightParams, GlobalLightParamsBlock, Light};
+pub use occluder::{
+    OccluderCircle, OccluderLine, OccluderLineVertex, OccluderRect, OccluderRotatedRect,
 };
 pub use occluder_batch::OccluderBatch;
 pub use pipeline::{LightPipeline, LightPipelineParams, NewLightPipelineError};
--- src/math/rect.rs
+++ src/math/rect.rs
@@ -130,7 +130,7 @@ impl RotatedRect {
         }
     }
 
-    pub fn corners(self) -> [Point2<f32>; 4] {
+    fn dxdy(self) -> (Vector2<f32>, Vector2<f32>) {
         let off_x = self.size.x / 2.0;
         let off_y = self.size.y / 2.0;
 
@@ -140,11 +140,43 @@ impl RotatedRect {
         let dx = Vector2::new(cos * off_x, sin * off_x);
         let dy = Vector2::new(-sin * off_y, cos * off_y);
 
+        (dx, dy)
+    }
+
+    pub fn corners(self) -> [Point2<f32>; 4] {
+        let (dx, dy) = self.dxdy();
+
         [
             self.center - dx - dy,
             self.center + dx - dy,
             self.center + dx + dy,
             self.center - dx + dy,
+        ]
+    }
+
+    pub fn lines(&self) -> [Line; 4] {
+        let corners = self.corners();
+
+        [
+            Line(corners[0], corners[1]),
+            Line(corners[1], corners[2]),
+            Line(corners[2], corners[3]),
+            Line(corners[3], corners[0]),
+        ]
+    }
+
+    pub fn caps(&self) -> [Line; 4] {
+        let corners = self.corners();
+        let (dx, dy) = self.dxdy();
+
+        let dx = (0.25 * dx.norm()).min(30.0) * dx.normalize();
+        let dy = (0.25 * dy.norm()).min(30.0) * dy.normalize();
+
+        [
+            Line(corners[0] + dx, corners[0] + dy),
+            Line(corners[1] - dx, corners[1] + dy),
+            Line(corners[2] - dx, corners[2] - dy),
+            Line(corners[3] + dx, corners[3] - dy),
         ]
     }
 }
