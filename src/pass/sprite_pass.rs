bced45bc6bad4992a11d9bff7564d6250d447b8a
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-01-11 19:01:35 +0100

    Bump version to 0.3.0

--- src/pass/sprite_pass.rs
+++ src/pass/sprite_pass.rs
@@ -63,8 +63,8 @@ impl SpritePass {
     where
         E: Element,
     {
-        #[cfg(feature = "coarse-prof")]
-        coarse_prof::profile!("SpritePass::draw");
+        //#[cfg(feature = "coarse-prof")]
+        //coarse_prof::profile!("SpritePass::draw");
 
         let mut sprite_infos = self.sprite_infos.borrow_mut();
         let sprite_info = sprite_infos.get(texture)?;
