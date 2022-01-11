841503f211919012471e6c3ad4f6c419480f1766
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-01-11 01:03:51 +0100

    Tuning gamma

--- src/light/compose_pass.rs
+++ src/light/compose_pass.rs
@@ -29,7 +29,8 @@ void main() {
     vec3 diffuse = albedo * (light + global_light_params.ambient);
     vec3 mapped = diffuse / (diffuse + vec3(1.0));
 
-    f_color = vec4(pow(mapped, vec3(1.0 / 2.2)), 1.0);
+    f_color = vec4(pow(diffuse, vec3(1.0 / 2.2)), 1.0);
+    //f_color = vec4(diffuse, 1.0);
 }
 "#;
 
