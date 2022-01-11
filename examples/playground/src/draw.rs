865fd2aeb2e5a1e25c9c9442515d5494e7e387fe
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-01-11 18:09:13 +0100

    Remove outlines (unused now)

--- examples/playground/src/draw.rs
+++ examples/playground/src/draw.rs
@@ -3,11 +3,11 @@ use nalgebra::{Matrix3, Point2, Point3, Vector2};
 
 use malen::{
     data::{
-        ColorCircle, ColorLineBatch, ColorRotatedRect, ColorTriangleBatch, ColorVertex,
-        InstanceBatch, Mesh, Sprite, SpriteBatch, TriangleTag,
+        ColorCircle, ColorRotatedRect, ColorTriangleBatch, ColorVertex, InstanceBatch, Mesh,
+        Sprite, SpriteBatch, TriangleTag,
     },
     geom::{Circle, Rect, Screen},
-    gl::{DepthTest, DrawParams, Texture, TextureParams, UniformBuffer},
+    gl::{Texture, TextureParams, UniformBuffer},
     light::{
         GlobalLightParams, Light, LightPipeline, LightPipelineParams, OccluderBatch,
         OccluderCircle, OccluderRect, OccluderRotatedRect,
@@ -34,7 +34,6 @@ pub struct Draw {
     shaded_color_batch: ColorTriangleBatch,
     shaded_sprite_batch: SpriteBatch,
     shaded_sprite_batch2: SpriteBatch,
-    outline_batch: ColorLineBatch,
     text_batch: TextBatch,
 
     light_pipeline: LightPipeline,
@@ -92,7 +91,6 @@ impl Draw {
         let shaded_color_batch = ColorTriangleBatch::new(context.gl())?;
         let shaded_sprite_batch = SpriteBatch::new(context.gl())?;
         let shaded_sprite_batch2 = SpriteBatch::new(context.gl())?;
-        let outline_batch = ColorLineBatch::new(context.gl())?;
         let text_batch = TextBatch::new(context.gl())?;
 
         let light_pipeline = LightPipeline::new(
@@ -118,7 +116,6 @@ impl Draw {
             shaded_color_batch,
             shaded_sprite_batch,
             shaded_sprite_batch2,
-            outline_batch,
             text_batch,
             light_pipeline,
             occluder_batch,
@@ -143,7 +140,6 @@ impl Draw {
         self.shaded_color_batch.clear();
         self.shaded_sprite_batch.clear();
         self.shaded_sprite_batch2.clear();
-        self.outline_batch.clear();
         self.text_batch.clear();
         self.occluder_batch.clear();
         self.lights.clear();
@@ -189,11 +185,6 @@ impl Draw {
                 (wall.rect().size / 50.0).component_mul(&self.texture2.size().cast::<f32>()),
             ),
         });
-        /*self.outline_batch.push(ColorRect {
-            rect: wall.rect(),
-            z: 0.2,
-            color: Color4::new(1.0, 1.0, 1.0, 1.0),
-        });*/
         self.occluder_batch.push(OccluderRect {
             rect: wall.rect(),
             ignore_light_index1: wall.lamp_index.map(|index| index as u32),
@@ -217,13 +208,6 @@ impl Draw {
             num_segments: 16,
             color: color.to_color4(),
         });
-        /*self.outline_batch.push(ColorCircle {
-            circle: enemy.circle(),
-            angle: 0.0,
-            z: 0.0,
-            num_segments: 16,
-            color: Color4::from_u8(255, 255, 255, 255),
-        });*/
         self.occluder_batch.push(OccluderCircle {
             circle: enemy.circle(),
             angle: 0.0,
@@ -237,7 +221,6 @@ impl Draw {
             angle: enemy.angle,
             angle_size: std::f32::consts::PI / 3.0,
             start: 18.0,
-            //color: color.scale(4.0),
             color: Color3::from_u8(212, 230, 135).to_linear(),
         });
     }
@@ -251,13 +234,6 @@ impl Draw {
             num_segments: 64,
             color: color.to_color4(),
         });
-        /*self.outline_batch.push(ColorCircle {
-            circle: ball.circle(),
-            angle: 0.0,
-            z: 0.0,
-            num_segments: 64,
-            color: Color4::from_u8(255, 255, 255, 255),
-        });*/
         self.occluder_batch.push(OccluderCircle {
             circle: ball.circle(),
             angle: 0.0,
@@ -284,13 +260,6 @@ impl Draw {
             num_segments: 64,
             color: color.to_color4(),
         });
-        /*self.outline_batch.push(ColorCircle {
-            circle: lamp.circle(),
-            angle: 0.0,
-            z: 0.0,
-            num_segments: 64,
-            color: Color4::from_u8(255, 255, 255, 255),
-        });*/
         self.lights.push(Light {
             position: Point3::new(lamp.pos.x, lamp.pos.y, 100.0),
             radius: 300.0,
@@ -308,11 +277,6 @@ impl Draw {
             z: 0.4,
             color: color.to_color4(),
         });
-        /*self.outline_batch.push(ColorRotatedRect {
-            rect: player.rotated_rect(),
-            z: 0.4,
-            color: Color4::new(1.0, 1.0, 1.0, 1.0),
-        });*/
         self.occluder_batch.push(OccluderRotatedRect {
             rect: player.rotated_rect(),
             ignore_light_index1: Some(self.lights.len() as u32),
@@ -334,13 +298,6 @@ impl Draw {
             start: 0.0,
             color: Color3::from_u8(150, 150, 150).to_linear(),
         });
-        /*self.lights.push(Light {
-            position: self.state.player.pos,
-            radius: 100.0,
-            angle: self.state.player.angle,
-            angle_size: 2.0 * std::f32::consts::PI,
-            color: Color3::new(0.8, 0.8, 2.0),
-        });*/
     }
 
     pub fn draw(&mut self, context: &mut Context, show_textures: bool) -> Result<(), FrameError> {
@@ -366,29 +323,6 @@ impl Draw {
                 ..GlobalLightParams::default()
             })
             .compose();
-
-        /*context.color_pass().draw(
-            &self.camera_matrices,
-            self.color_batch.draw_unit(),
-            &DrawParams {
-                depth_test: Some(DepthTest::default()),
-                ..DrawParams::default()
-            },
-        );*/
-        context.instanced_color_pass().draw(
-            &self.camera_matrices,
-            self.circle_instances.draw_unit(),
-            &DrawParams {
-                depth_test: Some(DepthTest::default()),
-                ..DrawParams::default()
-            },
-        );
-
-        context.color_pass().draw(
-            &self.camera_matrices,
-            self.outline_batch.draw_unit(),
-            &DrawParams::default(),
-        );
 
         self.font
             .draw(&self.screen_matrices, &mut self.text_batch)?;
