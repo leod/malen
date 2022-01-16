bb25c0bf75c2d7ed4e76fdeb63b0fa6354ffc3b2
leod <subtle.frustration@proton.me>
2022-01-16 21:59:26 +0100

    Rotated rect overlap resolution

--- src/geom/line.rs
+++ src/geom/line.rs
@@ -1,4 +1,10 @@
-use nalgebra::Point2;
+use nalgebra::{Point2, Vector2};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Line(pub Point2<f32>, pub Point2<f32>);
+
+impl Line {
+    pub fn delta(&self) -> Vector2<f32> {
+        self.1 - self.0
+    }
+}
