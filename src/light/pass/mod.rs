6d79f00e3361ad86293a5f1ce1ff32269b116df7
leod <subtle.frustration@proton.me>
2022-01-16 20:29:34 +0100

    Allow toggling indirect light

--- src/light/pass/mod.rs
+++ src/light/pass/mod.rs
@@ -1,9 +1,9 @@
 mod bindings;
 
 pub mod compose;
+pub mod compose_with_indirect;
 pub mod geometry_color;
 pub mod geometry_sprite_with_normals;
-pub mod reflector;
 pub mod screen_light;
 pub mod shaded_color;
 pub mod shaded_sprite;
