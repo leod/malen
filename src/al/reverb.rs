44961bc299355f495b0260e5e39800302971ccb2
leod <subtle.frustration@proton.me>
2022-01-22 16:49:44 +0100

    Simple reverb with convolution over existing impulse

--- src/al/reverb.rs
+++ src/al/reverb.rs
@@ -1,13 +1,35 @@
-use web_sys::AudioNode;
+use web_sys::{AudioNode, ConvolverNode};
 
 use super::{PlayError, Sound};
 
-fn convolver(impulse: &Sound) -> Result<AudioNode, PlayError> {
+pub fn convolver(impulse: &Sound) -> Result<ConvolverNode, PlayError> {
     let al = impulse.al();
-    let node = al
+    let convolver = al
         .context()
         .create_convolver()
         .map_err(PlayError::WebAudio)?;
+    convolver.set_buffer(Some(impulse.buffer()));
 
-    Ok(node.into())
+    Ok(convolver)
+}
+
+pub fn reverb(impulse: &Sound, dest: &AudioNode) -> Result<AudioNode, PlayError> {
+    let al = impulse.al();
+    let input = al.context().create_gain().map_err(PlayError::WebAudio)?;
+    let convolver = convolver(impulse)?;
+    let mix = al.context().create_gain().map_err(PlayError::WebAudio)?;
+
+    input
+        .connect_with_audio_node(&convolver)
+        .map_err(PlayError::WebAudio)?;
+    convolver
+        .connect_with_audio_node(&mix)
+        .map_err(PlayError::WebAudio)?;
+    input
+        .connect_with_audio_node(&mix)
+        .map_err(PlayError::WebAudio)?;
+    mix.connect_with_audio_node(dest)
+        .map_err(PlayError::WebAudio)?;
+
+    Ok(input.into())
 }
